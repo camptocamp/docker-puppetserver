59f4df6edd826df178af18060d9c3c02d597030c
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-10-01 16:35:36 +0200

    Fix image names in .build.sh and .publish.sh

--- .build.sh
+++ .build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Building image with tag latest"
-  docker build -t camptocamp/puppetboard:latest .
+  docker build -t camptocamp/puppetserver:latest .
 elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Building image with tag ${TRAVIS_TAG}"
-  docker build -t camptocamp/puppetboard:$TRAVIS_TAG .
+  docker build -t camptocamp/puppetserver:$TRAVIS_TAG .
 elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Building image with tag ${TRAVIS_BRANCH}"
-  docker build -t camptocamp/puppetboard:$TRAVIS_BRANCH .
+  docker build -t camptocamp/puppetserver:$TRAVIS_BRANCH .
 else
   echo "Don't know how to build image"
   exit 1
--- .publish.sh
+++ .publish.sh
@@ -3,13 +3,13 @@
 openssl aes-256-cbc -K $encrypted_264ff60bca14_key -iv $encrypted_264ff60bca14_iv -in .dockercfg.enc -out ~/.dockercfg -d
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Deploying image to docker hub for master (latest)"
-  docker push "camptocamp/puppetboard:latest"
+  docker push "camptocamp/puppetserver:latest"
 elif [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
-  docker push "camptocamp/puppetboard:${TRAVIS_TAG}"
+  docker push "camptocamp/puppetserver:${TRAVIS_TAG}"
 elif [ ! -z "$TRAVIS_BRANCH" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
-  docker push "camptocamp/puppetboard:${TRAVIS_BRANCH}"
+  docker push "camptocamp/puppetserver:${TRAVIS_BRANCH}"
 else
   echo "Not deploying image"
 fi
