941b076a86b368345d02c4137ef057aebb14485f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-10-09 10:25:46 +0200

    Fix when not setting MAX_ACTIVE_INSTANCES

--- puppetserver.sh
+++ puppetserver.sh
@@ -15,8 +15,8 @@ set $allow/@value true
 EOF
 fi
 
-if [ "${MAX_ACTIVE_INSTANCES}" -ge 1 ]; then
-  echo "Setting maximum number of JRuby instances to allow to ${MAX_ACTIVE_INSTANCE}"
+if [[ ! -z "${MAX_ACTIVE_INSTANCES}" && "${MAX_ACTIVE_INSTANCES}" -ge 1 ]]; then
+  echo "Setting maximum number of JRuby instances to allow to ${MAX_ACTIVE_INSTANCES}"
   cat << EOF | augtool -Ast "Trapperkeeper.lns incl /etc/puppetlabs/puppetserver/conf.d/*.conf"
 defnode jruby /files/etc/puppetlabs/puppetserver/conf.d/puppetserver.conf/@hash[.='jruby-puppet'] 'jruby-puppet'
 defnode max \$jruby/@simple[.='max-active-instances'] 'max-active-instances'
