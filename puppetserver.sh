97526165978d5ab93d86a63df2e8317d2c07e55c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-10-09 09:22:05 +0200

    Add debug

--- puppetserver.sh
+++ puppetserver.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [ "${EXTERNAL_SSL_TERMINATION}" ]; then
+  echo "Configuring Puppet server for External SSL Termination"
   # TODO: use augeas
   sed -i -e '/ssl-port/i \    port = 8080' -e '/ssl-host/i \    host = 0.0.0.0' /etc/puppetlabs/puppetserver/conf.d/webserver.conf
   echo -e "master: {\n    allow-header-cert-info: true\n}" > /etc/puppetlabs/puppetserver/conf.d/master.conf
 fi
 
 if [ "${MAX_ACTIVE_INSTANCES}" ]; then
+  echo "Setting maximum number of JRuby instances to allow to ${MAX_ACTIVE_INSTANCE}"
   # TODO: use augeas
   sed -i -E "s/^#?max-active-instances: .*$/max-active-instances: ${MAX_ACTIVE_INSTANCES}/" /etc/puppetlabs/puppetserver/conf.d/puppetserver.conf
 fi
