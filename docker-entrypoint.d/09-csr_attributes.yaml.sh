49de6f6ed9be8d085ef42a0e69ba33c1a61362a1
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-15 14:06:09 +0100

    Fix 09-csr_attributes.yaml.sh

--- docker-entrypoint.d/09-csr_attributes.yaml.sh
+++ docker-entrypoint.d/09-csr_attributes.yaml.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-if getent hosts rancher-metadata; then
-  echo $(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null) > /etc/puppetlabs/puppet/csr_attributes.yaml
+if getent hosts rancher-metadata > /dev/null; then
+  # Generate csr_attributes.yaml
+  cat << EOF > /etc/puppetlabs/puppet/csr_attributes.yaml
+---
+custom_attributes:
+  1.2.840.113549.1.9.7: '$(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null)'
+EOF
+
+  # Get certificate
+  if getent hosts puppetca > /dev/null; then
+    puppet agent -t --noop --server puppetca
+  else
+    puppet agent -t --noop
+  fi
 fi
