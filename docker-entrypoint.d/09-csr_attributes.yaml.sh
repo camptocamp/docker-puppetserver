99ed9bd651592f247790952dfa4e363df4c2edb7
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-15 10:27:13 +0100

    Add script to generate csr_attributes.yaml

---
+++ docker-entrypoint.d/09-csr_attributes.yaml.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo $(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null) > /etc/puppetlabs/puppet/csr_attributes.yaml
