7c789005bcdea052c188b7a263720bc5bd9d6bf9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-15 11:34:37 +0100

    check rancher-metadata

--- docker-entrypoint.d/09-csr_attributes.yaml.sh
+++ docker-entrypoint.d/09-csr_attributes.yaml.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo $(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null) > /etc/puppetlabs/puppet/csr_attributes.yaml
+if getent hosts rancher-metadata; then
+  echo $(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null) > /etc/puppetlabs/puppet/csr_attributes.yaml
+fi
