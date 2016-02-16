b7b38ee8f468a36c77a28525d9940a0427fbbe58
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-16 14:42:12 +0100

    Configure Subject Alternative Name in csr

--- docker-entrypoint.d/09-csr_attributes.yaml.sh
+++ docker-entrypoint.d/09-csr_attributes.yaml.sh
@@ -6,6 +6,8 @@ if getent hosts rancher-metadata > /dev/null && test -n "${CA}" && ! ${CA} ; the
 ---
 custom_attributes:
   1.2.840.113549.1.9.7: '$(curl http://rancher-metadata/latest/self/service/name 2> /dev/null):$(curl http://rancher-metadata/latest/self/service/uuid 2> /dev/null)'
+extension_requests:
+  2.5.29.17: $DNS_ALT_NAMES
 EOF
 
   # Get certificate
