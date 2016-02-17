bd20364e247e979ff4260a353a21700ca55ac9d8
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-17 17:31:21 +0100

    Remove trailing slash in gpg homedir

--- docker-entrypoint.d/11-gnupg.sh
+++ docker-entrypoint.d/11-gnupg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if test -n "${GPG_KEY}"; then
-  echo "${GPG_KEY}" | gpg --import --homedir /opt/puppetlabs/server/data/puppetserver/.gnupg/
-  chown -R puppet.puppet /opt/puppetlabs/server/data/puppetserver/.gnupg/
+  echo "${GPG_KEY}" | gpg --import --homedir /opt/puppetlabs/server/data/puppetserver/.gnupg
+  chown -R puppet.puppet /opt/puppetlabs/server/data/puppetserver/.gnupg
 fi
