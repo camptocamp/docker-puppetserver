7d350bb9f1cd55c8c6ce617e6dc27234b56ae8fa
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-17 17:21:56 +0100

    Fix .gnupg dir

--- docker-entrypoint.d/11-gnupg.sh
+++ docker-entrypoint.d/11-gnupg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if test -n "${GPG_KEY}"; then
-  echo "${GPG_KEY}" | gpg --import
+  echo "${GPG_KEY}" | gpg --import --homedir /opt/puppetlabs/server/data/puppetserver/.gnupg/
+  chown -R puppet.puppet /opt/puppetlabs/server/data/puppetserver/.gnupg/
 fi
