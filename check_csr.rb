ed8603fcb971312d38b16fb87e2f21c9e05b33cb
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-18 14:23:14 +0100

    Fix /check_csr

--- check_csr.rb
+++ check_csr.rb
@@ -14,11 +14,9 @@ exit 1 if challenge.nil?
 services = JSON.parse(open('http://rancher-metadata/latest/self/stack/services', 'Accept' => 'application/json').read)
 services.each do |s|
   if challenge == "#{s['name']}:#{s['uuid']}"
-    if csr.attributes[1].value.value.first.value.first.value[0].value
-      if ! csr.attributes.select { |a| a.oid == "ExtReq" }.nil?
-        # https://tickets.puppetlabs.com/browse/SERVER-1005
-        `puppet cert --allow-dns-alt-names --ssldir /etc/puppetlabs/puppet/ssl sign #{ARGV[0]}`
-      end
+    if ! csr.attributes.select { |a| a.oid == "ExtReq" }.nil?
+      # https://tickets.puppetlabs.com/browse/SERVER-1005
+      `puppet cert --allow-dns-alt-names --ssldir /etc/puppetlabs/puppet/ssl sign #{ARGV[0]}`
     end
     exit 0
   end
