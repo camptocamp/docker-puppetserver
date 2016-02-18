da9a58ad9cd2116bd85cc4344d08d571f2bb9990
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-18 10:29:59 +0100

    Set dns_alt_name in main section

--- docker-entrypoint.d/07-set-dns_alt_names.sh
+++ docker-entrypoint.d/07-set-dns_alt_names.sh
@@ -2,5 +2,5 @@
 
 if test -n "${DNS_ALT_NAMES}"; then
   echo "Configure dns_alt_names"
-  puppet config set dns_alt_names $DNS_ALT_NAMES --section agent
+  puppet config set dns_alt_names $DNS_ALT_NAMES --section main
 fi
