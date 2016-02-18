50cc22cd37a463d1276f32d81c4c24b144db8175
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-18 12:42:55 +0100

    Disable the internal Puppet CA Service if CA=false

--- docker-entrypoint.d/10-set-ca_server.sh
+++ docker-entrypoint.d/10-set-ca_server.sh
@@ -2,5 +2,5 @@
 
 if test -n "${CA}" && ! $CA; then
   puppet config set ca_server puppetca --section master
-  puppet config set ca false --section master
+  sed -i -e 's@^\(puppetlabs.services.ca.certificate-authority-service/certificate-authority-service\)@# \1@' -e 's@.*\(puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service\)@\1@' /etc/puppetlabs/puppetserver/bootstrap.cfg
 fi
