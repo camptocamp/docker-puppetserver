ba555e498f9b9f543b224486032c283caf0696bc
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-18 10:07:42 +0100

    Sign CSR with SANs manually

--- check_csr.rb
+++ check_csr.rb
@@ -6,13 +6,27 @@ require 'openssl'
 
 request = STDIN.read
 csr = OpenSSL::X509::Request.new(request)
-challenge = csr.attributes.select { |a| a.oid == "challengePassword" }.first.value.value.first.value 
+
+challenge = nil
+manual_sign = false
+csr.attributes.each do |a|
+  challenge = a.first.value.value.first.value if a.oid == "challengePassword"
+  manual_sign = true if a.oid == "ExtReq"
+end
 
 exit 1 if challenge.nil?
 
 services = JSON.parse(open('http://rancher-metadata/latest/self/stack/services', 'Accept' => 'application/json').read)
 services.each do |s|
-  exit 0 if challenge == "#{s['name']}:#{s['uuid']}"
+  if challenge == "#{s['name']}:#{s['uuid']}"
+    if csr.attributes[1].value.value.first.value.first.value[0].value
+      if manual_sign
+        # https://tickets.puppetlabs.com/browse/SERVER-1005
+        `puppet cert --allow-dns-alt-names --ssldir /etc/puppetlabs/puppet/ssl sign #{ARGV[0]}`
+      end
+    end
+    exit 0
+  end
 end
 
 exit 1
