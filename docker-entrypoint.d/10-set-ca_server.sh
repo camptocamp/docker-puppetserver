8e0a228858f79b2cb55cddc9974bb6b0f7014784
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-18 12:56:01 +0100

    Set ca_server in main section instead of master

--- docker-entrypoint.d/10-set-ca_server.sh
+++ docker-entrypoint.d/10-set-ca_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if test -n "${CA}" && ! $CA; then
-  puppet config set ca_server puppetca --section master
+  puppet config set ca_server puppetca --section main
   sed -i -e 's@^\(puppetlabs.services.ca.certificate-authority-service/certificate-authority-service\)@# \1@' -e 's@.*\(puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service\)@\1@' /etc/puppetlabs/puppetserver/bootstrap.cfg
 fi
