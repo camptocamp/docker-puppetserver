3471024df8c29f25e985cfafdbb7de231d0d8945
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-07-20 10:08:27 +0200

    Add comments to check_csr.rb

--- check_csr.rb
+++ check_csr.rb
@@ -38,12 +38,20 @@ challenge = csr.attributes.select { |a| a.oid == "challengePassword" }.first.val
 
 exit 3 if challenge.nil?
 
+# The old format (rancher only) takes only a value in the form "<service_name>:<service_uuid>"
+# The new format takes a name space in the form "<method>;<value>"
+# For this reason, the part before (and including) the ";" is optional:
+#   - with the old format, challenge_method=nil, challenge_value=<service_name>:<service_uuid>
+#   - with the new format, challenge_method=<method>, challenge_value=<value>
 challenge_method, challenge_value = challenge.match(/(?:([^;]+);)?(.+)/).captures()
 
+# We plan to add a "rancher" method to containers and get rid of the old format eventually
 if challenge_method == 'rancher' or challenge_method.nil?
   sign_rancher(csr, ARGV[0], challenge_value)
+# The "psk" method is used by Terraform
 elsif challenge_method == 'psk'
   sign_psk(csr, ARGV[0], challenge_value)
 end
+# In the future, add more methods (using e.g. AWS and OpenStack metadata)
 
 exit 4
