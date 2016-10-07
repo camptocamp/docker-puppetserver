1910c2ed6cdc70fdb8e9afb86704ee7cba264c8b
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-10-07 11:28:04 +0200

    Use Puppetserver 2.5.0

--- docker-entrypoint.d/10-set-ca_server.sh
+++ docker-entrypoint.d/10-set-ca_server.sh
@@ -3,7 +3,7 @@
 if test -n "${CA}" && ! $CA; then
   puppet config set ca_server puppetca --section main
   certname=$(puppet config print certname)
-  sed -i -e 's@^\(puppetlabs.services.ca.certificate-authority-service/certificate-authority-service\)@# \1@' -e 's@.*\(puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service\)@\1@' /etc/puppetlabs/puppetserver/bootstrap.cfg
+  sed -i -e 's@^\(puppetlabs.services.ca.certificate-authority-service/certificate-authority-service\)@# \1@' -e 's@.*\(puppetlabs.services.ca.certificate-authority-disabled-service/certificate-authority-disabled-service\)@\1@' /etc/puppetlabs/puppetserver/services.d/ca.cfg
   cat << EOF | augtool -Ast "Trapperkeeper.lns incl /etc/puppetlabs/puppetserver/conf.d/webserver.conf"
 defnode webserver /files/etc/puppetlabs/puppetserver/conf.d/webserver.conf/@hash[.='webserver'] webserver
 defnode sslcert \$webserver/@simple[.='ssl-cert'] ssl-cert
