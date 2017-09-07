9543fd132e91ef52c1227d8dff9f48a29f502053
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-09-07 16:26:15 +0200

    Allow to use AUTOSIGN_PSK

--- docker-entrypoint.d/09-csr_attributes.yaml.sh
+++ docker-entrypoint.d/09-csr_attributes.yaml.sh
@@ -18,4 +18,25 @@ EOF
     fi
     rc=$?
   done
+elif test -n "${AUTOSIGN_PSK}"; then
+  # Generate csr_attributes.yaml
+  cat << EOF > /etc/puppetlabs/puppet/csr_attributes.yaml
+---
+custom_attributes:
+  1.2.840.113549.1.9.7: 'hashed;$(CERTNAME=$(puppet config print certname) ruby -e 'require "openssl"; print Digest::SHA256.base64digest(ENV["AUTOSIGN_PSK"] + "/" + ENV["CERTNAME"] + "/puppet/production")')'
+extension_requests:
+  pp_role: puppet
+  pp_environment: production
+EOF
+
+  # Get certificate
+  rc=1
+  while test $rc -ne 0; do
+    if getent hosts puppetca > /dev/null; then
+      puppet agent -t --noop --server puppetca
+    else
+      puppet agent -t --noop
+    fi
+    rc=$?
+  done
 fi
