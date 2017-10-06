8dbea28f064c00b3aaac2e7c28b733b97cc420c5
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-10-06 14:59:07 +0200

    Remove riemann and graphite reporting support

--- docker-entrypoint.d/05-reports.sh
+++ docker-entrypoint.d/05-reports.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 
-reports=''
-
-if ! ${CA_ONLY} && getent hosts puppetdb > /dev/null 2>&1 ; then
+if getent hosts puppetdb > /dev/null 2>&1 ; then
   echo "Configure report to puppetdb"
-  test -n "${reports}" && reports="${reports},puppetdb" || reports="puppetdb"
+  puppet config set reports $reports --section master
 fi
-
-if getent hosts riemann > /dev/null 2>&1 ; then
-  echo "Configure report to riemann"
-  test -n "${reports}" && reports="${reports},riemann" || reports="riemann"
-  cat << EOF > $(puppet config print confdir)/riemann.yaml
----
-:riemann_server: 'riemann'
-:riemann_port: 5555
-EOF
-fi
-
-if getent hosts graphite > /dev/null 2>&1 ; then
-  echo "Configure report to graphite"
-  test -n "${reports}" && reports="${reports},graphite" || reports="graphite"
-  cat << EOF > $(puppet config print confdir)/graphite.yaml
----
-:graphite_server: 'graphite'
-:graphite_port: 2003
-EOF
-fi
-
-echo "reports=${reports}"
-test -n "${reports}" && puppet config set reports $reports --section master
-
-exit 0
