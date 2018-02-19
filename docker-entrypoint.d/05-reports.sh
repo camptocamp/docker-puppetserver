fd566aa656ffcc9615b72f0b505f7704f6a3dfd3
Mickaël Canévet <mickael.canevet@camptocamp.com>
2018-02-19 17:06:51 +0100

    Fix puppetdb reporter

--- docker-entrypoint.d/05-reports.sh
+++ docker-entrypoint.d/05-reports.sh
@@ -2,5 +2,5 @@
 
 if getent hosts puppetdb > /dev/null 2>&1 ; then
   echo "Configure report to puppetdb"
-  puppet config set reports $reports --section master
+  puppet config set reports puppetdb --section master
 fi
