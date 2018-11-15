7341799bbb58219abf37d881fbc82c0fdfca8985
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-11-15 14:37:41 +0100

    Get CA credentials from /run/secrets by default

--- docker-entrypoint.d/12-create-ca.sh
+++ docker-entrypoint.d/12-create-ca.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [ ! -e /etc/puppetlabs/puppet/ssl/ca/inventory.txt ]; then
+CA_DIR=$(puppet config print cadir)
+
+: ${CA_KEY:=/run/secrets/ca_key.pem}
+: ${CA_CRT:=/run/secrets/ca_crt.pem}
+: ${CA_CRL:=/run/secrets/ca_crl.pem}
+
+if [ ! -e "${CA_DIR}/inventory.txt" ]; then
   echo "Importing CA"
-  puppetserver ca import --private-key /ca_key.pem --cert-bundle /ca_crt.pem --crl-chain /ca_crl.pem
-  chmod g=u /etc/puppetlabs/puppet/ssl/ca/*
+  puppetserver ca import --private-key "${CA_KEY}" \
+	                     --cert-bundle "${CA_CRT}" \
+						 --crl-chain "${CA_CRL}"
+  chmod -R g=u ${CA_DIR}/*
 else
   echo "CA is already imported. Clear it if you need to import again."
 fi
