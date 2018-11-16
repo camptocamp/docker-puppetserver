04c578d83ce89950a7f81f2c0968ec499dcaac9a
Mickaël Canévet <mickael.canevet@camptocamp.com>
2018-11-16 10:14:08 +0100

    Fix file detection

--- docker-entrypoint.d/11-gnupg.sh
+++ docker-entrypoint.d/11-gnupg.sh
@@ -2,7 +2,7 @@
 
 : ${GPG_KEYFILE:=/run/secrets/gpg.asc}
 
-if test -n "${GPG_KEYFILE}"; then
+if test -f "${GPG_KEYFILE}"; then
   cat "${GPG_KEYFILE}" | gpg --import --homedir /opt/puppetlabs/server/data/puppetserver/.gnupg
   #chown -R puppet.puppet /opt/puppetlabs/server/data/puppetserver/.gnupg
 fi
